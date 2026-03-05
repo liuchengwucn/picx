b6a48171a1cb6adfba6604f949001815b9e86756
Liu Chengwu <liuchengwu@pku.edu.cn>
2026-03-05 18:28:42 +0800

    refactor: remove todos router from tRPC

--- src/integrations/trpc/router.ts
+++ src/integrations/trpc/router.ts
@@ -1,30 +1,11 @@
 import type { TRPCRouterRecord } from "@trpc/server";
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "./init";
+import { createTRPCRouter } from "./init";
 import { paperRouter } from "./routers/paper";
 import { sseRouter } from "./routers/sse";
 import { uploadRouter } from "./routers/upload";
 import { userRouter } from "./routers/user";
 
-const todos = [
-	{ id: 1, name: "Get groceries" },
-	{ id: 2, name: "Buy a new phone" },
-	{ id: 3, name: "Finish the project" },
-];
-
-const todosRouter = {
-	list: publicProcedure.query(() => todos),
-	add: publicProcedure
-		.input(z.object({ name: z.string() }))
-		.mutation(({ input }) => {
-			const newTodo = { id: todos.length + 1, name: input.name };
-			todos.push(newTodo);
-			return newTodo;
-		}),
-} satisfies TRPCRouterRecord;
-
 export const trpcRouter = createTRPCRouter({
-	todos: todosRouter,
 	user: userRouter,
 	paper: paperRouter,
 	upload: uploadRouter,
