db34b4f839117d8a4ab6dc1f49fcfc369bd04207
Liu Chengwu <liuchengwu@pku.edu.cn>
2026-03-14 21:24:47 +0800

    feat: add robots.txt route pointing to sitemap

---
+++ src/routes/robots[.txt].tsx
@@ -0,0 +1,25 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+async function handler({ request }: { request: Request }) {
+  const origin = new URL(request.url).origin;
+
+  const content = `User-agent: *
+Allow: /
+
+Sitemap: ${origin}/sitemap.xml`;
+
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Cache-Control": "public, max-age=86400",
+    },
+  });
+}
+
+export const Route = createFileRoute("/robots[.txt]")({
+  server: {
+    handlers: {
+      GET: handler,
+    },
+  },
+});
